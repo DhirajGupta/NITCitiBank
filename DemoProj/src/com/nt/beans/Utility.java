b39a5975e9d01c7057359ba768e159dd4fbe4060
DHIRAJ <TL@gmail.com>
2020-04-10 23:33:54 +0530

    TL created m2 method

--- DemoProj/src/com/nt/beans/Utility.java
+++ DemoProj/src/com/nt/beans/Utility.java
@@ -10,5 +10,7 @@ public class Utility {
 		System.out.println("Utility m1() method:1111");
 		System.out.println("abc123");
 	}
-
+	public void m2() {
+		System.out.println("Utility m2() method");
+	}
 }
